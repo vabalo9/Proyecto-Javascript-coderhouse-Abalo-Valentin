a9091b5d926403c45a145934edf50daa52a77691
Abalo Valentin <valentinabalo9@gmail.com>
2022-11-14 17:41:26 -0300

    Entrega protecto numero 2

--- js/main.js
+++ js/main.js
@@ -1,58 +1,47 @@
 alert("Bienvenido a Rebomba lencería e indumentaría")
-  alert("Podemos ofrecerte tanto boxer, como conjuntos, remeras o medias.")
   let valor = ""
+  
 
+  let productos = [
+    {  id: 1, articulo: "conjuntos", modelo:"Conjunto  Andressa 1218", precio: 1600 },
+    {  id: 2, articulo: "conjuntos", modelo:"Conjunto  Andressa 1221", precio: 1100 },
+    {  id: 3, articulo: "boxers", modelo:"BOXER DUFOUR ALGODON RAYADO SIN COSTURA - ART. 11881", precio: 1600 },
+    {  id: 4, articulo: "boxers", modelo:"BOXER DOUFOUR ALGODON BATIK SIN COSTURA - ART. 11831", precio: 1900 },
+    {  id: 5, articulo: "medias", modelo:"MEDIA CORTA LISA ALGODON- ART. 3327", precio: 900 },
+    {  id: 6, articulo: "medias", modelo:"MEDIA 3/4 CON LYCRA ELEGANCE - ART. 2098", precio: 1150 },
+    {  id: 7, articulo: "camisetas", modelo:"CAMISETA FRISADA ULTRA-TERMICA - ART. 11795", precio: 850 },
+    {  id: 8, articulo: "camisetas", modelo:"CAMISETA PIJAMA - ART. 11797", precio: 2000 },
+    ]
+ 
+    
 do {
- valor = prompt("Ingresa la palabra boxers, para saber el precio de los boxers. La palabra conjuntos para saber el precio de los conjuntos. palabra remeras, para saber el precio de las remeras o la palabra medias para saber el precio de las medias. Si desea salir por favor escriba la palabra salir ").toLowerCase()
- console.log(valor) 
-  if (valor.toLowerCase() == "boxers") {
-    alert ("EL precio de los boxer es de $1500")
-      let confirmacion= prompt("Desea comprar boxers? ingrese 1 si desea comprar o 2 si no desea comprar")
-      if (confirmacion == "1") {
-        let cantidad = Number(prompt("que cantidad desea comprar"))
-        let total =cantidad * 1500
-        alert("En total serían " + cantidad + " Boxers por un precio de $" + total)
-      }
-  }
+    valor = prompt(("Ingrese el tipo de articulo que desea comprar:\nboxers\nconjuntos\ncamisetas\nmedias\n Si quiere salir ingrese la palabra salir ").toLowerCase())
+     if (valor == "boxers" || valor == "medias" || valor == "camisetas" || valor == "conjuntos" ) {
+        let respuesta = productos.filter(producto => producto.articulo === valor)
 
-  else if (valor == "conjuntos") {
-    alert("el precio de los conjuntos es de $3200")
-      let confirme= prompt("Desea comprar conjuntos? ingrese 1 si desea comprar o 2 si no desea comprar")
-      if (confirme == "1") {
-        let cantidad = Number(prompt("que cantidad desea comprar"))
-        let total =cantidad * 3500
-        alert("En total serían " + cantidad + " Conjuntos por un precio de $" + total)
+        let salida = ""
+       for (const producto of respuesta) { 
+        salida = salida + producto.modelo + "\n Precio: $ " + producto.precio + "\npara comprarlo acontinuación presione " + producto.id + "\n\n"
+       }
+       alert("Articulos encontrados:\n" + salida)
+       
+       let articuloFinal = parseFloat(prompt("Ingrese el número del producto que desea comprar"))
+       let compra = productos.find(producto => producto.id === articuloFinal)
+       let final= ""
+       
+        final = "Usded compro el articulo " + compra.modelo + "\n Por un total de: $ " + compra.precio 
+       
+       alert(final)
+       alert("Gracias por su compra")
+      
         
-      }
-  }
-
-  else if (valor == "remeras") {
-    alert("el precio de las remeras es de $2400")
-      let confirme= prompt("Desea comprar conjuntos? ingrese 1 si desea comprar o 2 si no desea comprar")
-      if (confirme == "1") {
-        let cantidad = Number(prompt("que cantidad desea comprar"))
-        let total =cantidad * 2400
-        alert("En total serían " + cantidad + " Remeras por un precio de $" + total)
-        
-      }
-  }
-
-  else if (valor == "medias") {
-    alert("el precio de las medias es de $900")
-      let confirme= prompt("Desea comprar medias? ingrese 1 si desea comprar o 2 si no desea comprar")
-      if (confirme == "1") {
-        let cantidad = Number(prompt("cuantos pares desea comprar"))
-        let total =cantidad * 900
-        alert("En total serían " + cantidad + " Remeras por un precio de $" + total)
-        
-      }
-  }
-  else if (valor == "salir") {
-
-  }
-  else {
-    alert("Lo sentimos el producto que ingreso no se encuentra dentro de nuestro stock en este momento")
-  }
-
-
-} while (valor != "salir")
+     }
+     else if (valor == "salir") {
+   
+     }
+     else {
+       alert("Lo sentimos el producto que ingreso no se encuentra dentro de nuestro stock en este momento")
+     }
+   
+   
+   } while (valor != "salir")
