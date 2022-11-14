1e795a4cf2584d2d4914c612d97f33ae973a0682
Abalo Valentin <valentinabalo9@gmail.com>
2022-11-14 17:58:52 -0300

    Pequeñas modificaciones proyecto numero 2

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ alert("Bienvenido a Rebomba lencería e indumentaría")
     {  id: 1, articulo: "conjuntos", modelo:"Conjunto  Andressa 1218", precio: 1600 },
     {  id: 2, articulo: "conjuntos", modelo:"Conjunto  Andressa 1221", precio: 1100 },
     {  id: 3, articulo: "boxers", modelo:"BOXER DUFOUR ALGODON RAYADO SIN COSTURA - ART. 11881", precio: 1600 },
-    {  id: 4, articulo: "boxers", modelo:"BOXER DOUFOUR ALGODON BATIK SIN COSTURA - ART. 11831", precio: 1900 },
+    {  id: 4, articulo: "boxers", modelo:"BOXER DUFOUR ALGODON BATIK SIN COSTURA - ART. 11831", precio: 1900 },
     {  id: 5, articulo: "medias", modelo:"MEDIA CORTA LISA ALGODON- ART. 3327", precio: 900 },
     {  id: 6, articulo: "medias", modelo:"MEDIA 3/4 CON LYCRA ELEGANCE - ART. 2098", precio: 1150 },
     {  id: 7, articulo: "camisetas", modelo:"CAMISETA FRISADA ULTRA-TERMICA - ART. 11795", precio: 850 },
@@ -15,13 +15,13 @@ alert("Bienvenido a Rebomba lencería e indumentaría")
  
     
 do {
-    valor = prompt(("Ingrese el tipo de articulo que desea comprar:\nboxers\nconjuntos\ncamisetas\nmedias\n Si quiere salir ingrese la palabra salir ").toLowerCase())
+    valor = prompt(("Ingrese el tipo de articulo que desea comprar:\nboxers\nconjuntos\ncamisetas\nmedias\n Para terminar  ingrese la palabra salir ").toLowerCase())
      if (valor == "boxers" || valor == "medias" || valor == "camisetas" || valor == "conjuntos" ) {
         let respuesta = productos.filter(producto => producto.articulo === valor)
 
         let salida = ""
        for (const producto of respuesta) { 
-        salida = salida + producto.modelo + "\n Precio: $ " + producto.precio + "\npara comprarlo acontinuación presione " + producto.id + "\n\n"
+        salida = salida + producto.modelo + "\n Precio: $ " + producto.precio + "\npara comprarlo a continuación presione " + producto.id + "\n\n"
        }
        alert("Articulos encontrados:\n" + salida)
        
