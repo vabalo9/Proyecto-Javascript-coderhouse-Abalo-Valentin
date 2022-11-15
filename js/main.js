7cdc02dbff148e8d710c582d9357492725cf17ba
Abalo Valentin <valentinabalo9@gmail.com>
2022-11-15 17:01:39 -0300

    correciones de 2da entrega

--- js/main.js
+++ js/main.js
@@ -12,27 +12,38 @@ alert("Bienvenido a Rebomba lencería e indumentaría")
     {  id: 7, articulo: "camisetas", modelo:"CAMISETA FRISADA ULTRA-TERMICA - ART. 11795", precio: 850 },
     {  id: 8, articulo: "camisetas", modelo:"CAMISETA PIJAMA - ART. 11797", precio: 2000 },
     ]
- 
+    
+    function filter(lista, comparador){
+      let respuesta = lista.filter(producto => producto.articulo === comparador)
+      return respuesta
+    }
+    function find(lista, comparador){
+      let compra = lista.find(producto => producto.id === comparador)
+      let final= ""
+      final = "Usded compro el articulo " + compra.modelo + "\n Por un total de: $ " + compra.precio 
+      return final
+    }
+    function despedida(mensaje){
+      alert(mensaje)
+       alert("Gracias por su compra")
+       valor = "salir"
+    }
+
+    function menu() {
     
 do {
     valor = prompt("Ingrese el tipo de articulo que desea comprar:\nboxers\nconjuntos\ncamisetas\nmedias\n Para terminar  ingrese la palabra salir ").toLowerCase()
      if (valor == "boxers" || valor == "medias" || valor == "camisetas" || valor == "conjuntos" ) {
-        let respuesta = productos.filter(producto => producto.articulo === valor)
-
+        
+        
         let salida = ""
-       for (const producto of respuesta) { 
+       for (const producto of filter(productos,valor)) { 
         salida = salida + producto.modelo + "\n Precio: $ " + producto.precio + "\npara comprarlo a continuación presione " + producto.id + "\n\n"
        }
        let articuloFinal = parseFloat(prompt("Articulos encontrados:\n" + salida))
        
-       let compra = productos.find(producto => producto.id === articuloFinal)
-       let final= ""
        
-        final = "Usded compro el articulo " + compra.modelo + "\n Por un total de: $ " + compra.precio 
-       
-       alert(final)
-       alert("Gracias por su compra")
-       valor = "salir"
+       despedida(find(productos ,articuloFinal));
       
         
      }
@@ -45,3 +56,6 @@ do {
    
    
    } while (valor != "salir")
+  }
+
+  menu();
